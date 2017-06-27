066f3943c4e1effdffb55f9add46782edf0aab1d
Gabriela Parra <parragabriela@icloud.com>
2017-06-27 11:28:49 -0400

    added a form for users to submit new products

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+                                //'environment' variable, set by the systems
 
   // render the error page
   res.status(err.status || 500);
--- routes/product-routes.js
+++ routes/product-routes.js
@@ -18,8 +18,24 @@ router.get('/products', (req, res, next) => {
     // the views folder
     res.render('product-views/products-list-view.ejs', {
       productsAndStuff: productResults
-      // transfer of information to the view 
+      // transfer of information to the view
     });
+  });
+});
+
+//STEP #1 of form submission for a new product
+router.get('/products/new', (req, res, next) => {
+  res.render('product-views/new-product-view.ejs');
+});
+
+//STEP #2 of form submission for a new product
+// <form method="post" action="/products">
+router.post('/products', (req, res, next) => {
+  const theProduct = new ProductModel ({
+    name: req.body.productName,
+    price: req.body.productPrice,
+    imageUrl: req.body.productImageUrl,
+    description: req.body.productDescription
   });
 });
 
